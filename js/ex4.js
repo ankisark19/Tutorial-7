e13e3bfc3bca9d422420d12ee5365f06c48ca44d
Ankita Sarkar <ankita.sarkar01@student.csulb.edu>
2021-03-03 14:21:09 -0800

    New change

--- js/ex4.js
+++ js/ex4.js
@@ -3,17 +3,20 @@
 // Text is center aligned
 
 //Table Creation using documnet.createElement() method
-const tableElement = document.createElement("TABLE");
+const tableElement = document.createElement("table");
 
 //Create rows and cells
 for (i = 1; i <=12; i++){
   const tableRow = document.createElement("tr")
   const tableCell = document.createElement("tc");
   tableCell.innerText = i;
+  
+  // Add Cell border
+  tableCell.style.border="1px solid black"
 
 // Blue background for numbers divisible by 4
 if(i%4 === 0){
-  tableCell.style.backgroundColor="blue";
+  tableCell.style.backgroundColor="lightblue";
 }
 
 //Adding data to row
@@ -24,10 +27,10 @@ tableElement.appendChild(tableRow);
 }
 
 //Add divisibility element
-const divElement = document.getElementById("table");
+const divElement = document.getElementById("output");
 divElement.appendChild(tableElement);
 
 // Add style to table
-const styleElement = document.getElementById("table");
-styleElement.style.border = "2px solid black";
-styleElement.style.textAlign = "center";
+//const styleElement = document.getElementById("table");
+tableElement.style.border = "1px solid black";
+tableElement.style.textAlign = "center";
